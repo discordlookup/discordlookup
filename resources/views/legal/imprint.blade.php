c64bdc4d83e3f2787407a674583daf9090fe0da6
fbrettnich <github@fbrettnich.de>
2022-12-22 20:17:26 +0100

    Moved imprint to env
    Signed-off-by: fbrettnich <github@fbrettnich.de>

--- resources/views/legal/imprint.blade.php
+++ resources/views/legal/imprint.blade.php
@@ -6,21 +6,29 @@
 @section('robots', 'noindex, nofollow')
 
 @section('content')
-
     <div class="container mt-5">
         <div class="page-header mt-2">
             <h2 class="pb-2 fw-bold">{{ __('Imprint') }}</h2>
         </div>
 
         <div class="user-select-none">
-            @if(\Illuminate\Support\Facades\View::exists('legal.content.imprint'))
-                @include('legal.content.imprint')
-            @endif
+            <h3>{{ __('Legal Provider Identification:') }}</h3>
+            {{ env('LEGAL_FIRSTNAME') }} {{ env('LEGAL_LASTNAME') }}<br>
+            {{ env('LEGAL_STREET') }} {{ env('LEGAL_STREET_NUMBER') }}<br>
+            {{ env('LEGAL_ZIPCODE') }} {{ env('LEGAL_CITY') }}<br>
+            {{ env('LEGAL_COUNTRY') }}<br>
+            <br>
+            {{ __('Phone') }}: <a href="tel:{{ env('LEGAL_PHONE') }}">{{ env('LEGAL_PHONE') }}</a><br>
+            {{ __('E-Mail') }}: <a href="mailto:{{ env('LEGAL_EMAIL') }}">{{ env('LEGAL_EMAIL') }}</a><br>
+            <br>
+            <b>{!! __('Responsible for content according to &sect; 18 para. 2 MStV:') !!}</b><br>
+            {{ env('LEGAL_FIRSTNAME') }} {{ env('LEGAL_LASTNAME') }}<br>
+            {{ env('LEGAL_STREET') }} {{ env('LEGAL_STREET_NUMBER') }}<br>
+            {{ env('LEGAL_ZIPCODE') }} {{ env('LEGAL_CITY') }}<br>
+            {{ env('LEGAL_COUNTRY') }}<br>
         </div>
-
         <br>
         <br>
-
         <h3>{{ __('Disclaimer') }}</h3>
         <div>
             {{ __('DiscordLookup is not affiliated, associated, authorized, endorsed by, or in anyway officially connected with Discord Inc., or any of its subsidiaries or its affiliates.') }}
@@ -28,5 +36,4 @@
             {{ __('Any new products or features discovered are subject to change and not guaranteed to release.') }}
         </div>
     </div>
-
 @endsection
