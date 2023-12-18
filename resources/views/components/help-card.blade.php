b0177d455c13b19d30781590ad436d7e81a1077e
Felix <github@fbrettnich.de>
2023-12-18 17:19:37 +0100

    Updated help slug generation
    Signed-off-by: Felix <github@fbrettnich.de>

--- resources/views/components/help-card.blade.php
+++ resources/views/components/help-card.blade.php
@@ -1,8 +1,8 @@
 <div class="flex flex-col rounded shadow-sm bg-discord-gray-1 overflow-hidden">
-    <div class="py-4 px-5 lg:px-6 w-full flex items-center border-b border-discord-gray-4" id="{{ strtolower(preg_replace(['/\s+/', '/[^a-zA-Z-]/'], ['-', ''], $title)) }}">
+    <div class="py-4 px-5 lg:px-6 w-full flex items-center border-b border-discord-gray-4" id="{{ Str::slug($title) }}">
         <h3 class="font-semibold text-xl">
             {{ $title }}
-            <a href="#{{ strtolower(preg_replace(['/\s+/', '/[^a-zA-Z-]/'], ['-', ''], $title)) }}" class="text-discord-blurple opacity-75 hover:opacity-50">#</a>
+            <a href="#{{ Str::slug($title) }}" class="text-discord-blurple opacity-75 hover:opacity-50">#</a>
         </h3>
     </div>
     <div class="p-5 lg:p-6 grow w-full">
